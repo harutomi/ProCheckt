665052cc916582a3d69151809487bb8802e36e03
富永治明 <tominagaharuaki@MacBook-Pro.local>
2017-08-16 20:59:55 +0900

    カラム作成完了ver1

--- db/migrate/20170816082103_create_users.rb
+++ db/migrate/20170816082103_create_users.rb
@@ -3,6 +3,8 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :name
       t.string :department
+      t.text :expertise
+      t.text :profile
       t.timestamps
     end
   end
